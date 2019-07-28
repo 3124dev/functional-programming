920039e593989b1131f083ddb280197e556a6b42
3124dev <3124dev@naver.com>
2019-07-28 19:59:52 +0900

    [Update] pipe, go

--- _.js
+++ _.js
@@ -45,6 +45,31 @@
 //   return list;
 // }
 
+// curry
+function _curry(fn) {
+  return function(a, b) {
+    // 인자가 2개 들어오면 한 번에 바로 리턴하고
+    // 그렇지 않으면 미뤄두었다가 다음번 재 호출 때 리턴한다.
+    // Javascript의 클로져를 이용
+    return arguments.length == 2 ? fn(a, b) : function(b) { return fn(a, b); };
+  }
+}
+
+// curryr
+function _curryr(fn) {
+  return function(a, b) {
+    // 인자가 2개 들어오면 기존 curry와 같은 순서로 가며
+    // 그렇지 않은 경우 기존 curry와 다른 순서로 간다.
+    return arguments.length == 2 ? fn(a, b) : function(b) { return fn(b, a); };
+  }
+}
+
+// curryr_get
+var _get = _curryr(function(obj, key) {
+  return obj == null ? undefined : obj[key];
+});
+
+
 function _map(list, mapper) {
   let new_list = [];
   _each(list, (val) => new_list.push(mapper(val)));
